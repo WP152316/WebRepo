107b1111d887007c2a0129f87a314efdad8fcbfe
WP152316 <didtmdcjf254@naver.com>
2017-10-10 11:25:19 +0900

    Add files via upload

---
+++ src/org/dimigo/servlet/HelloServlet.java
@@ -0,0 +1,87 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(description = "첫번째 서블릿", urlPatterns = { "/Hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//요청 데이터 처리
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		System.out.printf("id : %s, name : %s\n",id,name);
+		
+		//출력데이터
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Servlet Tset</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello, Servlet</h1>");
+		out.println("<h2>안녕, 서블릿</h2>");
+		out.println("<h2>id :" + id + ", name : " + name + "</h2>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.print("doPost() 호출됨");
+		doGet(request, response);
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#init()
+	 */
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init()");
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#service(javax.servlet.ServletRequest, javax.servlet.ServletResponse)
+	 */
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException {
+		System.out.println("service()");
+		super.service(req,resp);
+	}
+	/**
+	 * 
+	 */
+	private void destroy() {
+		System.out.println("destroy()1111");
+	}
+}
---
+++ src/org/dimigo/servlet/LogoutServlet.java
@@ -0,0 +1,53 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getSession().getAttribute("user") == null) {
+			response.sendRedirect("jsp/login.jsp");
+		}
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("/jsp/sessionInfo.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 세션에 담긴 사용자 정보를 삭제
+		HttpSession session = request.getSession();
+		// session.removeAttribute("user");
+		session.invalidate();
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/login.jsp");
+		rd.forward(request, response);
+	}
+
+}
